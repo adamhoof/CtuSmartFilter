63a65a9d07f97a1362b1293c14026ae4ee80df79
adamhoof <adam.hofman1@gmail.com>
2024-11-29 17:25:27 +0100

    Simplify MqttClient and make it more robust, take out sample WiFi code

--- include/MqttClient.h
+++ include/MqttClient.h
@@ -6,6 +6,7 @@ extern bool reconnectMqtt;
 extern uint32_t lastReconnect;
 
 void connectMqttClient();
+
 void onMqttConnect(bool sessionPresent);
 void onMqttDisconnect(espMqttClientTypes::DisconnectReason reason);
 void onMqttSubscribe(uint16_t packetId, const espMqttClientTypes::SubscribeReturncode* codes, size_t len);
