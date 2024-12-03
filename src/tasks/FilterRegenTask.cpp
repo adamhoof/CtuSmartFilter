e00a7a20f35d88258c85c10a0467a6a1191c5b63
adamhoof <adam.hofman1@gmail.com>
2024-12-03 17:08:01 +0100

    Improve FilterRegenTask

--- src/tasks/FilterRegenTask.cpp
+++ src/tasks/FilterRegenTask.cpp
@@ -1,67 +1,70 @@
 #include "tasks/FilterRegenTask.h"
 #include <freertos/task.h>
 
-namespace
+bool isCo2LevelHigh(const CO2Sensor& co2Sensor, const uint32_t co2Threshold)
 {
-    bool isCo2LevelHigh(const CO2Sensor& co2Sensor, const uint32_t co2Threshold)
-    {
-        const Measurement co2Measurement = co2Sensor.getCO2Value();
-        return co2Measurement.value > co2Threshold;
-    }
+    const Measurement co2Measurement = co2Sensor.getCO2Value();
+    return co2Measurement.value > co2Threshold;
+}
 
-    void regulateHeatingPad(const FilterRegenTaskParams* params)
-    {
-        TickType_t heatingPadDuration = pdMS_TO_TICKS(params->config.heatingPadDurationMs);
-        TickType_t startTime = xTaskGetTickCount();
-        params->heatingPad.setPower(255);
+void regulateHeatingPad(const FilterRegenTaskParams* params)
+{
+    TickType_t heatingPadDuration = pdMS_TO_TICKS(params->config.heatingPadDurationMs);
+    TickType_t startTime = xTaskGetTickCount();
+    params->heatingPad.setPower(255);
 
-        while (xTaskGetTickCount() - startTime < heatingPadDuration) {
-            Measurement temperatureMeasurement = params->thermocoupleSensor.getTemperatureValue();
-            double currentTemp = temperatureMeasurement.value;
+    while (xTaskGetTickCount() - startTime < heatingPadDuration) {
+        Measurement temperatureMeasurement = params->thermocoupleSensor.getTemperatureValue();
+        double currentTemp = temperatureMeasurement.value;
 
-            if (currentTemp < params->config.heatingPadTargetTemp) {
-                params->heatingPad.setPower(255);
-            }
-            else {
-                params->heatingPad.setPower(128);
-            }
-
-            vTaskDelay(pdMS_TO_TICKS(1000));
+        if (currentTemp < params->config.heatingPadTargetTemp) {
+            params->heatingPad.setPower(255);
+        }
+        else {
+            params->heatingPad.setPower(128);
         }
 
-        params->heatingPad.setPower(0);
+        vTaskDelay(pdMS_TO_TICKS(1000));
     }
 
-    void regenerateFilter(const FilterRegenTaskParams* params)
-    {
-        params->fan.turnOff();
-        regulateHeatingPad(params);
+    params->heatingPad.setPower(0);
+}
+
+void regenerateFilter(const FilterRegenTaskParams* params)
+{
+    params->fan.turnOff();
+    regulateHeatingPad(params);
+}
+
+void waitUntilCO2IsAcceptable(const FilterRegenTaskParams* params)
+{
+    params->fan.runAtMax();
+    while (isCo2LevelHigh(params->co2Sensor, params->config.heatingPadDurationMs)) {
+        vTaskDelay(pdMS_TO_TICKS(3000));
     }
 }
 
 void filterRegenTask(void* parameter)
 {
     const auto* params = static_cast<FilterRegenTaskParams*>(parameter);
+    //TODO make this react to changes in state?
+    const TickType_t periodDuration = pdMS_TO_TICKS(params->config.levelPeriods[params->config.currentLevel - 1]);
 
     while (true) {
-        const TickType_t periodDuration = pdMS_TO_TICKS(params->config.levelPeriods[params->config.currentLevel - 1]);
         const TickType_t periodStartTime = xTaskGetTickCount();
 
         while (xTaskGetTickCount() - periodStartTime < periodDuration) {
-            const bool co2IsHigh = isCo2LevelHigh(params->co2Sensor, params->config.co2Threshold);
-
-            if (co2IsHigh) {
-                params->fan.runAtMax();
-            }
-            else {
-                params->fan.runAtIdle();
-            }
+            isCo2LevelHigh(params->co2Sensor, params->config.co2Threshold)
+                ? params->fan.runAtMax()
+                : params->fan.runAtIdle();
 
             vTaskDelay(pdMS_TO_TICKS(3000));
         }
 
-        if (!isCo2LevelHigh(params->co2Sensor, params->config.co2Threshold)) {
-            regenerateFilter(params);
+        if (isCo2LevelHigh(params->co2Sensor, params->config.co2Threshold)) {
+            waitUntilCO2IsAcceptable(params);
         }
+
+        regenerateFilter(params);
     }
 }
