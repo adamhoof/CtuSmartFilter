fc5ffba1ea0c218d15116621f3669ca1cd3c984b
adamhoof <adam.hofman1@gmail.com>
2025-06-18 20:39:06 +0200

    Remove incorrect reassignment

--- src/WifiEventHandlers.cpp
+++ src/WifiEventHandlers.cpp
@@ -5,7 +5,6 @@ const int MAX_WIFI_DISCONNECTS = 10;
 
 void wifiConnectedEventHandler(const arduino_event_id_t event_id)
 {
-    wifiDisconnectCount = 0;
     Serial.println("WiFi connected");
     Serial.println("IP address: ");
     Serial.println(WiFi.localIP());
